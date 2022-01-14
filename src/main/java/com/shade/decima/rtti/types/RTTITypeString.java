37eea265bd170c74047c942eaa6c207354dbb9f5
ShadelessFox <yanuari.channel@gmail.com>
2022-01-14 13:48:34 +0300

    Empty string read/write fix

--- src/main/java/com/shade/decima/rtti/types/RTTITypeString.java
+++ src/main/java/com/shade/decima/rtti/types/RTTITypeString.java
@@ -1,7 +1,7 @@
 package com.shade.decima.rtti.types;
 
-import com.shade.decima.rtti.RTTIType;
 import com.shade.decima.rtti.RTTIDefinition;
+import com.shade.decima.rtti.RTTIType;
 import com.shade.decima.util.IOUtils;
 import com.shade.decima.util.NotNull;
 import com.shade.decima.util.hash.CRC32C;
@@ -21,16 +21,22 @@ public final class RTTITypeString implements RTTIType<String> {
     @Override
     public String read(@NotNull ByteBuffer buffer) {
         final int size = buffer.getInt();
-        final int hash = buffer.getInt();
-        return new String(IOUtils.getBytesExact(buffer, size, hash), StandardCharsets.UTF_8);
+        if (size > 0) {
+            final int hash = buffer.getInt();
+            return new String(IOUtils.getBytesExact(buffer, size, hash), StandardCharsets.UTF_8);
+        } else {
+            return "";
+        }
     }
 
     @Override
     public void write(@NotNull ByteBuffer buffer, @NotNull String value) {
         final byte[] data = value.getBytes(StandardCharsets.UTF_8);
         buffer.putInt(data.length);
-        buffer.putInt(CRC32C.calculate(data));
-        buffer.put(data);
+        if (data.length > 0) {
+            buffer.putInt(CRC32C.calculate(data));
+            buffer.put(data);
+        }
     }
 
     @NotNull
