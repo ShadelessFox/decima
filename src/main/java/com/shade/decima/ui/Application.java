e557de5ad9a56c3b280db20928364b64b7c08d89
ShadelessFox <yanuari.channel@gmail.com>
2022-01-16 02:52:42 +0300

    #5 Show dialog with unhandled exception

--- src/main/java/com/shade/decima/ui/Application.java
+++ src/main/java/com/shade/decima/ui/Application.java
@@ -3,10 +3,17 @@ package com.shade.decima.ui;
 import com.formdev.flatlaf.FlatLightLaf;
 import com.formdev.flatlaf.extras.FlatInspector;
 import com.formdev.flatlaf.extras.FlatUIDefaultsInspector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             UIManager.put("TitlePane.unifiedBackground", false);
@@ -26,6 +33,24 @@ public class Application {
             frame.setLocationRelativeTo(null);
             frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
             frame.setVisible(true);
+
+            Thread.setDefaultUncaughtExceptionHandler((thread, exception) -> {
+                final StringWriter sw = new StringWriter();
+                final PrintWriter pw = new PrintWriter(sw);
+                exception.printStackTrace(pw);
+
+                final JScrollPane pane = new JScrollPane(new JTextArea(sw.toString().replace("\t", "    ")));
+                pane.setPreferredSize(new Dimension(640, 480));
+
+                JOptionPane.showMessageDialog(
+                    frame,
+                    pane,
+                    "An error occurred during program execution",
+                    JOptionPane.ERROR_MESSAGE
+                );
+
+                log.error("Unhandled exception", exception);
+            });
         });
     }
 }
