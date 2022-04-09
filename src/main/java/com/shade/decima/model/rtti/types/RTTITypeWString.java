9efc27a4688aa43fdc1f3f1e5325b68b250bce0d
ShadelessFox <yanuari.channel@gmail.com>
2022-04-09 18:35:03 +0300

    Don't read checksum for `WString`-s because they don't have it

--- src/main/java/com/shade/decima/model/rtti/types/RTTITypeWString.java
+++ src/main/java/com/shade/decima/model/rtti/types/RTTITypeWString.java
@@ -4,7 +4,6 @@ import com.shade.decima.model.rtti.RTTIDefinition;
 import com.shade.decima.model.rtti.registry.RTTITypeRegistry;
 import com.shade.decima.model.util.IOUtils;
 import com.shade.decima.model.util.NotNull;
-import com.shade.decima.model.util.hash.CRC32C;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
@@ -19,15 +18,17 @@ public class RTTITypeWString extends RTTITypeString {
     @Override
     public String read(@NotNull RTTITypeRegistry registry, @NotNull ByteBuffer buffer) {
         final int size = buffer.getInt() * 2;
-        final int hash = buffer.getInt();
-        return new String(IOUtils.getBytesExact(buffer, size, hash), StandardCharsets.UTF_16);
+        if (size > 0) {
+            return new String(IOUtils.getBytesExact(buffer, size), StandardCharsets.UTF_16);
+        } else {
+            return "";
+        }
     }
 
     @Override
     public void write(@NotNull RTTITypeRegistry registry, @NotNull ByteBuffer buffer, @NotNull String value) {
         final byte[] data = value.getBytes(StandardCharsets.UTF_16);
         buffer.putInt(data.length / 2);
-        buffer.putInt(CRC32C.calculate(data));
         buffer.put(data);
     }
 }
