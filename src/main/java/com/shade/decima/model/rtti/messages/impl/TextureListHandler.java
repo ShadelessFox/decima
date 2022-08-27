31557e6be2245b112c46855dccc2c0a2750fbfa3
ShadelessFox <yanuari.channel@gmail.com>
2022-08-27 17:35:04 +0300

    Add MsgReadBinary handler for `TextureList`

---
+++ src/main/java/com/shade/decima/model/rtti/messages/impl/TextureListHandler.java
@@ -0,0 +1,42 @@
+package com.shade.decima.model.rtti.messages.impl;
+
+import com.shade.decima.model.base.GameType;
+import com.shade.decima.model.rtti.RTTIUtils;
+import com.shade.decima.model.rtti.messages.RTTIMessageHandler;
+import com.shade.decima.model.rtti.messages.RTTIMessageReadBinary;
+import com.shade.decima.model.rtti.objects.RTTIObject;
+import com.shade.decima.model.rtti.registry.RTTITypeRegistry;
+import com.shade.decima.model.rtti.types.RTTITypeArray;
+import com.shade.decima.model.rtti.types.RTTITypeClass;
+import com.shade.util.NotNull;
+
+import java.nio.ByteBuffer;
+import java.util.Objects;
+
+@RTTIMessageHandler(type = "TextureList", message = "MsgReadBinary", game = GameType.DS)
+public class TextureListHandler implements RTTIMessageReadBinary {
+    @Override
+    public void read(@NotNull RTTITypeRegistry registry, @NotNull RTTIObject object, @NotNull ByteBuffer buffer) {
+        final RTTITypeClass TextureListEntry = RTTIUtils.newClassBuilder(registry, "Texture").build();
+        final RTTITypeClass Texture = (RTTITypeClass) registry.find("Texture");
+
+        final RTTIMessageReadBinary textureHandler = Objects.requireNonNull(Texture.getMessageHandler("MsgReadBinary"));
+
+        final int count = buffer.getInt();
+        final RTTIObject[] textures = new RTTIObject[count];
+
+        for (int i = 0; i < count; i++) {
+            final RTTIObject entry = TextureListEntry.instantiate();
+            textureHandler.read(registry, entry, buffer);
+
+            textures[i] = entry;
+        }
+
+        object.define("Textures", new RTTITypeArray<>("Array", TextureListEntry), textures);
+    }
+
+    @Override
+    public void write(@NotNull RTTITypeRegistry registry, @NotNull RTTIObject object, @NotNull ByteBuffer buffer) {
+        throw new IllegalStateException("Not implemented");
+    }
+}
