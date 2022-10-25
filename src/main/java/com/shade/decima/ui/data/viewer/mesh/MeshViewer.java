54f2b99e680377aaad1a9ee43b3cc6604a1dcada
REDxEYE <med45c@gmail.com>
2022-10-25 21:48:45 +0300

    Add new ValueViewerRegistration to mesh viewer

--- src/main/java/com/shade/decima/ui/data/viewer/mesh/MeshViewer.java
+++ src/main/java/com/shade/decima/ui/data/viewer/mesh/MeshViewer.java
@@ -1,15 +1,20 @@
 package com.shade.decima.ui.data.viewer.mesh;
 
+import com.shade.decima.model.base.GameType;
 import com.shade.decima.ui.data.ValueViewer;
+import com.shade.decima.ui.data.registry.Type;
+import com.shade.decima.ui.data.registry.ValueViewerRegistration;
 import com.shade.decima.ui.editor.core.CoreEditor;
 import com.shade.platform.ui.editors.Editor;
 import com.shade.util.NotNull;
 
 import javax.swing.*;
 
+@ValueViewerRegistration({
+    @Type(name = "MeshResourceBase", game = GameType.DS),
+    @Type(name = "ArtPartsDataResource", game = GameType.DS)
+})
 public class MeshViewer implements ValueViewer {
-    public static final MeshViewer INSTANCE = new MeshViewer();
-
     @NotNull
     @Override
     public JComponent createComponent() {
