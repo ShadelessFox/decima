f4ae0de62491e8b15eb366fecb7181be36bbcd28
REDxEYE <med45c@gmail.com>
2022-11-02 01:39:31 +0300

    Remove unused fields

--- src/main/java/com/shade/decima/ui/data/viewer/mesh/ModelExportContext.java
+++ src/main/java/com/shade/decima/ui/data/viewer/mesh/ModelExportContext.java
@@ -1,22 +1,18 @@
 package com.shade.decima.ui.data.viewer.mesh;
 
-import com.shade.decima.model.rtti.objects.RTTIObject;
 import com.shade.decima.ui.data.viewer.mesh.dmf.DMFSceneFile;
 import com.shade.util.NotNull;
 
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ModelExportContext {
     public int depth = 0;
-    DMFSceneFile scene;
-    Path outputDir;
-    String resourceName;
-    boolean embedBuffers;
+    public DMFSceneFile scene;
+    public Path outputDir;
+    public String resourceName;
+    public boolean embedBuffers;
     public boolean embedTextures = false;
-    boolean exportTextures = false;
-    boolean convertVertices = false;
+    public boolean exportTextures = false;
 
     public ModelExportContext(@NotNull String resourceName, @NotNull Path outputDir) {
         this.resourceName = resourceName;
