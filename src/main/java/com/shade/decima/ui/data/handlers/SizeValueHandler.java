1ce6218b596c6244904b46c689fd6209c287f628
ShadelessFox <yanuari.channel@gmail.com>
2022-12-13 23:31:51 +0100

    Core Editor: Implement decorator for `{I}Size`

---
+++ src/main/java/com/shade/decima/ui/data/handlers/SizeValueHandler.java
@@ -0,0 +1,26 @@
+package com.shade.decima.ui.data.handlers;
+
+import com.shade.decima.model.rtti.RTTIType;
+import com.shade.decima.model.rtti.objects.RTTIObject;
+import com.shade.decima.ui.data.registry.Type;
+import com.shade.decima.ui.data.registry.ValueHandlerRegistration;
+import com.shade.platform.ui.controls.TextAttributes;
+import com.shade.util.NotNull;
+import com.shade.util.Nullable;
+
+@ValueHandlerRegistration(id = "size", name = "Size", value = {
+    @Type(name = "ISize"),
+    @Type(name = "FSize")
+})
+public class SizeValueHandler extends ObjectValueHandler {
+    @Nullable
+    @Override
+    public Decorator getDecorator(@NotNull RTTIType<?> type) {
+        return (value, component) -> {
+            final RTTIObject obj = (RTTIObject) value;
+            final String text = "%sx%s".formatted(obj.get("Width"), obj.get("Height"));
+
+            component.append(text, TextAttributes.REGULAR_ATTRIBUTES);
+        };
+    }
+}
