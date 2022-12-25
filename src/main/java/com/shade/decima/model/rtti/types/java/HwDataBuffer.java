17d414090eac83a46b898232be1987e6eb1f5f65
ShadelessFox <yanuari.channel@gmail.com>
2022-12-25 23:10:15 +0100

    RTTI: Add support for non-streaming `DataBufferResource` buffers

--- src/main/java/com/shade/decima/model/rtti/types/java/HwDataBuffer.java
+++ src/main/java/com/shade/decima/model/rtti/types/java/HwDataBuffer.java
@@ -4,13 +4,14 @@ import com.shade.decima.model.rtti.objects.RTTIObject;
 import com.shade.decima.model.rtti.registry.RTTITypeRegistry;
 import com.shade.decima.model.rtti.types.RTTITypeEnum;
 import com.shade.decima.ui.data.registry.Type;
+import com.shade.platform.model.util.IOUtils;
 import com.shade.util.NotNull;
 
 import java.nio.ByteBuffer;
 
 public class HwDataBuffer {
     @RTTIField(type = @Type(name = "uint32"))
-    public int size;
+    public int count;
     @RTTIField(type = @Type(name = "ERenderDataStreamingMode"))
     public RTTITypeEnum.Constant mode;
     @RTTIField(type = @Type(name = "uint32"))
@@ -21,35 +22,42 @@ public class HwDataBuffer {
     public int stride;
     @RTTIField(type = @Type(type = HwDataSource.class))
     public RTTIObject dataSource;
+    @RTTIField(type = @Type(name = "Array<uint8>"))
+    public byte[] data;
 
     @NotNull
     public static RTTIObject read(@NotNull RTTITypeRegistry registry, @NotNull ByteBuffer buffer) {
         final var object = new HwDataBuffer();
-        object.size = buffer.getInt();
+        object.count = buffer.getInt();
         object.mode = registry.<RTTITypeEnum>find("ERenderDataStreamingMode").valueOf(buffer.getInt());
         object.flags = buffer.getInt();
         object.format = registry.<RTTITypeEnum>find("EDataBufferFormat").valueOf(buffer.getInt());
         object.stride = buffer.getInt();
 
-        if (!object.mode.toString().equals("Streaming")) {
-            throw new IllegalStateException("Unsupported data buffer mode: " + object.mode);
+        switch (object.mode.toString()) {
+            case "Streaming" -> object.dataSource = HwDataSource.read(registry, buffer);
+            case "NotStreaming" -> object.data = IOUtils.getBytesExact(buffer, object.count * object.stride);
+            default -> throw new IllegalStateException("Unsupported buffer mode: " + object.mode);
         }
-
-        object.dataSource = HwDataSource.read(registry, buffer);
 
         return new RTTIObject(registry.find(HwDataBuffer.class), object);
     }
 
     public void write(@NotNull RTTITypeRegistry registry, @NotNull ByteBuffer buffer) {
-        buffer.putInt(size);
+        buffer.putInt(count);
         buffer.putInt(mode.value());
         buffer.putInt(flags);
         buffer.putInt(format.value());
         buffer.putInt(stride);
-        dataSource.<HwDataSource>cast().write(registry, buffer);
+
+        if (dataSource != null) {
+            dataSource.<HwDataSource>cast().write(registry, buffer);
+        } else {
+            buffer.put(data);
+        }
     }
 
     public int getSize() {
-        return 20 + dataSource.<HwDataSource>cast().getSize();
+        return 20 + (dataSource != null ? dataSource.<HwDataSource>cast().getSize() : data.length);
     }
 }
