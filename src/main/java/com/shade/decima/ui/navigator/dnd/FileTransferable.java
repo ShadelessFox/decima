01d0c1d10ec99b77ed9e1a3e0362355176c0fbd7
ShadelessFox <yanuari.channel@gmail.com>
2023-01-04 06:18:08 +0100

    Navigator: Copy files upon DnD completion, not upon gesture recognition

--- src/main/java/com/shade/decima/ui/navigator/dnd/FileTransferable.java
+++ src/main/java/com/shade/decima/ui/navigator/dnd/FileTransferable.java
@@ -8,28 +8,22 @@ import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.Closeable;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 public class FileTransferable implements Transferable, Closeable {
-    private final Path directory;
-    private final List<Path> files;
+    private final List<NavigatorFileNode> nodes;
+    private volatile Data data;
 
-    public FileTransferable(@NotNull List<NavigatorFileNode> nodes) throws IOException {
-        this.directory = Files.createTempDirectory("decima-dnd");
-        this.files = new ArrayList<>();
-
-        for (NavigatorFileNode node : nodes) {
-            final Packfile packfile = node.getPackfile();
-            final Path file = Files.createFile(directory.resolve(node.getLabel()));
-            final byte[] bytes = packfile.extract(node.getHash());
-
-            Files.write(file, bytes);
-            files.add(file);
-        }
+    public FileTransferable(@NotNull List<NavigatorFileNode> nodes) {
+        this.nodes = nodes;
     }
 
     @Override
@@ -39,24 +33,58 @@ public class FileTransferable implements Transferable, Closeable {
 
     @Override
     public boolean isDataFlavorSupported(DataFlavor flavor) {
-        return flavor.equals(DataFlavor.javaFileListFlavor);
+        return DataFlavor.javaFileListFlavor.equals(flavor);
     }
 
     @Override
-    public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException {
-        if (isDataFlavorSupported(flavor)) {
-            return files.stream().map(Path::toFile).toList();
+    public synchronized Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+        if (DataFlavor.javaFileListFlavor.equals(flavor)) {
+            if (data == null) {
+                data = Data.from(nodes);
+            }
+
+            return data.files;
         }
 
         throw new UnsupportedFlavorException(flavor);
     }
 
     @Override
-    public void close() throws IOException {
-        for (Path file : files) {
-            Files.delete(file);
+    public synchronized void close() throws IOException {
+        if (data != null) {
+            data.delete();
+            data = null;
+        }
+    }
+
+    private record Data(@NotNull List<Path> paths, @NotNull List<File> files) {
+        @NotNull
+        public static Data from(@NotNull List<NavigatorFileNode> nodes) throws IOException {
+            final Path dir = Files.createTempDirectory("decima-dnd");
+            final List<File> files = new ArrayList<>();
+            final List<Path> paths = new ArrayList<>();
+
+            for (NavigatorFileNode node : nodes) {
+                final Packfile packfile = node.getPackfile();
+                final Path file = dir.resolve(node.getLabel());
+
+                try (InputStream is = packfile.newInputStream(node.getHash())) {
+                    Files.copy(is, file, REPLACE_EXISTING);
+                }
+
+                files.add(file.toFile());
+                paths.add(file);
+            }
+
+            paths.add(dir);
+
+            return new Data(paths, files);
         }
 
-        Files.delete(directory);
+        public void delete() throws IOException {
+            for (Path path : paths) {
+                Files.deleteIfExists(path);
+            }
+        }
     }
 }
