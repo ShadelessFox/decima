a3baf74247ada6cbee1d5c2e66a36c765c028a26
ShadelessFox <yanuari.channel@gmail.com>
2023-01-30 01:24:31 +0100

    RTTI: Implement `LocalizedTextResource` handler for HZD

---
+++ src/main/java/com/shade/decima/model/rtti/types/ds/DSLocalizedText.java
@@ -0,0 +1,57 @@
+package com.shade.decima.model.rtti.types.ds;
+
+import com.shade.decima.model.rtti.objects.RTTIObject;
+import com.shade.decima.model.rtti.registry.RTTITypeRegistry;
+import com.shade.decima.model.rtti.types.java.HwLocalizedText;
+import com.shade.decima.model.rtti.types.java.RTTIField;
+import com.shade.decima.ui.data.registry.Type;
+import com.shade.platform.model.util.IOUtils;
+import com.shade.util.NotNull;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+public class DSLocalizedText implements HwLocalizedText {
+    @RTTIField(type = @Type(name = "String"))
+    public String text;
+    @RTTIField(type = @Type(name = "String"))
+    public String notes;
+    @RTTIField(type = @Type(name = "uint8"))
+    public byte flags;
+
+    @NotNull
+    public static RTTIObject read(@NotNull RTTITypeRegistry registry, @NotNull ByteBuffer buffer) {
+        final var object = new DSLocalizedText();
+        object.text = IOUtils.getString(buffer, buffer.getShort());
+        object.notes = IOUtils.getString(buffer, buffer.getShort());
+        object.flags = buffer.get();
+
+        return new RTTIObject(registry.find(DSLocalizedText.class), object);
+    }
+
+    @Override
+    public void write(@NotNull RTTITypeRegistry registry, @NotNull ByteBuffer buffer) {
+        final byte[] text = this.text.getBytes(StandardCharsets.UTF_8);
+        final byte[] notes = this.notes.getBytes(StandardCharsets.UTF_8);
+
+        buffer.putShort((short) text.length);
+        buffer.put(text);
+        buffer.putShort((short) notes.length);
+        buffer.put(notes);
+        buffer.put(flags);
+    }
+
+    @Override
+    public int getSize() {
+        final byte[] text = this.text.getBytes(StandardCharsets.UTF_8);
+        final byte[] notes = this.notes.getBytes(StandardCharsets.UTF_8);
+
+        return 5 + text.length + notes.length;
+    }
+
+    @NotNull
+    @Override
+    public String getText() {
+        return text;
+    }
+}
---
+++ src/main/java/com/shade/decima/model/rtti/types/java/HwLocalizedText.java
@@ -0,0 +1,8 @@
+package com.shade.decima.model.rtti.types.java;
+
+import com.shade.util.NotNull;
+
+public interface HwLocalizedText extends HwType {
+    @NotNull
+    String getText();
+}
