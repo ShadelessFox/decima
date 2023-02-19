e50935c36aca41ef154a8a64c1403ba9f2416cb8
ShadelessFox <yanuari.channel@gmail.com>
2023-02-19 01:48:45 +0100

    Core Editor: Implement utility for inspecting the selected value

---
+++ src/main/java/com/shade/decima/ui/editor/core/menu/InspectValueItem.java
@@ -0,0 +1,33 @@
+package com.shade.decima.ui.editor.core.menu;
+
+import com.shade.decima.ui.Application;
+import com.shade.decima.ui.data.ValueController;
+import com.shade.decima.ui.data.ValueController.EditType;
+import com.shade.decima.ui.editor.core.CoreEditor;
+import com.shade.decima.ui.editor.core.dialog.InspectValueDialog;
+import com.shade.platform.ui.PlatformDataKeys;
+import com.shade.platform.ui.menus.MenuItem;
+import com.shade.platform.ui.menus.MenuItemContext;
+import com.shade.platform.ui.menus.MenuItemRegistration;
+import com.shade.util.NotNull;
+
+import java.util.Objects;
+
+import static com.shade.decima.ui.menu.MenuConstants.*;
+
+@MenuItemRegistration(parent = CTX_MENU_CORE_EDITOR_ID, name = "Inspect Value", icon = "Action.searchIcon", group = CTX_MENU_CORE_EDITOR_GROUP_GENERAL, order = 3000)
+public class InspectValueItem extends MenuItem {
+    @Override
+    public void perform(@NotNull MenuItemContext ctx) {
+        final CoreEditor editor = (CoreEditor) ctx.getData(PlatformDataKeys.EDITOR_KEY);
+        final ValueController<Object> controller = Objects.requireNonNull(editor.getValueController(EditType.INLINE));
+
+        new InspectValueDialog<>(editor.getInput().getProject(), controller).showDialog(Application.getFrame());
+    }
+
+    @Override
+    public boolean isVisible(@NotNull MenuItemContext ctx) {
+        return ctx.getData(PlatformDataKeys.EDITOR_KEY) instanceof CoreEditor editor
+            && editor.getValueController(EditType.INLINE) != null;
+    }
+}
