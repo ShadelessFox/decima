3c328809a7c587219a8de59d76364adcb4e2fd50
mithkr <49768797+mithkr@users.noreply.github.com>
2023-04-03 22:45:34 +0200

    add support for 2DArray texture to png export
    2DArray texture get exported to png as a vertical strip of all slices

--- src/main/java/com/shade/decima/ui/data/viewer/texture/exporter/TextureExporterPNG.java
+++ src/main/java/com/shade/decima/ui/data/viewer/texture/exporter/TextureExporterPNG.java
@@ -4,6 +4,8 @@ import com.shade.decima.ui.data.viewer.texture.TextureExporter;
 import com.shade.decima.ui.data.viewer.texture.controls.ImageProvider;
 import com.shade.util.NotNull;
 
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
 import javax.imageio.ImageIO;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -15,7 +17,13 @@ public class TextureExporterPNG implements TextureExporter {
     @Override
     public void export(@NotNull ImageProvider provider, @NotNull Set<Option> options, @NotNull WritableByteChannel channel) throws IOException {
         final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(provider.getImage(0, 0), "png", baos);
+        final BufferedImage concatImage = new BufferedImage(provider.getMaxWidth(), provider.getMaxHeight()*provider.getSliceCount(0), BufferedImage.TYPE_INT_ARGB);
+        final Graphics2D g2d = concatImage.createGraphics();
+        for (int slice = 0; slice < provider.getSliceCount(0); slice++) {
+            g2d.drawImage(provider.getImage(0, slice), 0, provider.getMaxHeight()*slice, null);
+        }
+        g2d.dispose();
+        ImageIO.write(concatImage, "png", baos);
 
         final ByteBuffer buffer = ByteBuffer.wrap(baos.toByteArray());
         channel.write(buffer);
