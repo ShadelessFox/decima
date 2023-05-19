6a3b8e0b98b1750b90d7a444f4de5270036a876e
ShadelessFox <yanuari.channel@gmail.com>
2023-05-20 00:38:01 +0200

    Texture Viewer: Add missing DDS pixel formats

--- modules/decima-ui/src/main/java/com/shade/decima/ui/data/viewer/texture/exporter/TextureExporterDDS.java
+++ modules/decima-ui/src/main/java/com/shade/decima/ui/data/viewer/texture/exporter/TextureExporterDDS.java
@@ -41,22 +41,110 @@ public class TextureExporterDDS implements TextureExporter {
     private static final int DDS_DIMENSION_TEXTURE3D = 4;
     private static final int DDS_RESOURCE_MISC_TEXTURECUBE = 0x4;
 
-    // Only seen formats are listed here. No urge to add all other formats now
-    // https://learn.microsoft.com/en-us/windows/win32/api/dxgiformat/ne-dxgiformat-dxgi_format
-    private static final Map<String, Integer> DXGI_MAPPINGS = Map.ofEntries(
-        Map.entry("RGBA_8888", 28),
-        Map.entry("R_UNORM_8", 61),
-        Map.entry("R_UNORM_16", 56),
-        Map.entry("BC1", 71),
-        Map.entry("BC2", 74),
-        Map.entry("BC3", 77),
-        Map.entry("BC4U", 80),
-        Map.entry("BC4S", 81),
-        Map.entry("BC5U", 83),
-        Map.entry("BC5S", 84),
-        Map.entry("BC6U", 95),
-        Map.entry("BC6S", 96),
-        Map.entry("BC7", 98)
+    private static final int DXGI_FORMAT_R32G32B32A32_FLOAT = 2;
+    private static final int DXGI_FORMAT_R32G32B32A32_UINT = 3;
+    private static final int DXGI_FORMAT_R32G32B32A32_SINT = 4;
+    private static final int DXGI_FORMAT_R32G32B32_FLOAT = 6;
+    private static final int DXGI_FORMAT_R16G16B16A16_FLOAT = 10;
+    private static final int DXGI_FORMAT_R16G16B16A16_UNORM = 11;
+    private static final int DXGI_FORMAT_R16G16B16A16_UINT = 12;
+    private static final int DXGI_FORMAT_R16G16B16A16_SNORM = 13;
+    private static final int DXGI_FORMAT_R16G16B16A16_SINT = 14;
+    private static final int DXGI_FORMAT_R32G32_FLOAT = 16;
+    private static final int DXGI_FORMAT_R32G32_UINT = 17;
+    private static final int DXGI_FORMAT_R32G32_SINT = 18;
+    private static final int DXGI_FORMAT_R10G10B10A2_UNORM = 24;
+    private static final int DXGI_FORMAT_R11G11B10_FLOAT = 26;
+    private static final int DXGI_FORMAT_R8G8B8A8_UNORM = 28;
+    private static final int DXGI_FORMAT_R8G8B8A8_UINT = 30;
+    private static final int DXGI_FORMAT_R8G8B8A8_SNORM = 31;
+    private static final int DXGI_FORMAT_R8G8B8A8_SINT = 32;
+    private static final int DXGI_FORMAT_R16G16_FLOAT = 34;
+    private static final int DXGI_FORMAT_R16G16_UNORM = 35;
+    private static final int DXGI_FORMAT_R16G16_UINT = 36;
+    private static final int DXGI_FORMAT_R16G16_SNORM = 37;
+    private static final int DXGI_FORMAT_R16G16_SINT = 38;
+    private static final int DXGI_FORMAT_R32_FLOAT = 41;
+    private static final int DXGI_FORMAT_R32_UINT = 42;
+    private static final int DXGI_FORMAT_R32_SINT = 43;
+    private static final int DXGI_FORMAT_R8G8_UINT = 50;
+    private static final int DXGI_FORMAT_R8G8_SNORM = 51;
+    private static final int DXGI_FORMAT_R8G8_SINT = 52;
+    private static final int DXGI_FORMAT_R16_FLOAT = 54;
+    private static final int DXGI_FORMAT_R16_UNORM = 56;
+    private static final int DXGI_FORMAT_R16_UINT = 57;
+    private static final int DXGI_FORMAT_R16_SNORM = 58;
+    private static final int DXGI_FORMAT_R16_SINT = 59;
+    private static final int DXGI_FORMAT_R8_UNORM = 61;
+    private static final int DXGI_FORMAT_R8_UINT = 62;
+    private static final int DXGI_FORMAT_R8_SNORM = 63;
+    private static final int DXGI_FORMAT_R8_SINT = 64;
+    private static final int DXGI_FORMAT_R8G8_B8G8_UNORM = 68;
+    private static final int DXGI_FORMAT_BC1_UNORM = 71;
+    private static final int DXGI_FORMAT_BC2_UNORM = 74;
+    private static final int DXGI_FORMAT_BC3_UNORM = 77;
+    private static final int DXGI_FORMAT_BC4_UNORM = 80;
+    private static final int DXGI_FORMAT_BC4_SNORM = 81;
+    private static final int DXGI_FORMAT_BC5_UNORM = 83;
+    private static final int DXGI_FORMAT_BC5_SNORM = 84;
+    private static final int DXGI_FORMAT_BC6H_UF16 = 95;
+    private static final int DXGI_FORMAT_BC6H_SF16 = 96;
+    private static final int DXGI_FORMAT_BC7_UNORM = 98;
+
+    @SuppressWarnings("RedundantTypeArguments")
+    private static final Map<String, Integer> DXGI_MAPPINGS = Map.<String, Integer>ofEntries(
+        Map.entry("RGBA_8888", DXGI_FORMAT_R8G8B8A8_UNORM),
+        Map.entry("RGBA_FLOAT_32", DXGI_FORMAT_R32G32B32A32_FLOAT),
+        Map.entry("RGB_FLOAT_32", DXGI_FORMAT_R32G32B32_FLOAT),
+        Map.entry("RG_FLOAT_32", DXGI_FORMAT_R32G32_FLOAT),
+        Map.entry("R_FLOAT_32", DXGI_FORMAT_R32_FLOAT),
+        Map.entry("RGBA_FLOAT_16", DXGI_FORMAT_R16G16B16A16_FLOAT),
+        Map.entry("RGB_FLOAT_16", DXGI_FORMAT_R16G16B16A16_FLOAT),
+        Map.entry("RG_FLOAT_16", DXGI_FORMAT_R16G16_FLOAT),
+        Map.entry("R_FLOAT_16", DXGI_FORMAT_R16_FLOAT),
+        Map.entry("RGBA_UNORM_32", DXGI_FORMAT_R32G32B32A32_UINT),
+        Map.entry("RGBA_UNORM_16", DXGI_FORMAT_R16G16B16A16_UNORM),
+        Map.entry("RG_UNORM_16", DXGI_FORMAT_R16G16_UNORM),
+        Map.entry("R_UNORM_16", DXGI_FORMAT_R16_UNORM),
+        Map.entry("RGBA_UNORM_8", DXGI_FORMAT_R8G8B8A8_UNORM),
+        Map.entry("RG_UNORM_8", DXGI_FORMAT_R8G8_B8G8_UNORM),
+        Map.entry("R_UNORM_8", DXGI_FORMAT_R8_UNORM),
+        Map.entry("RGBA_NORM_16", DXGI_FORMAT_R16G16B16A16_SNORM),
+        Map.entry("RG_NORM_16", DXGI_FORMAT_R16G16_SNORM),
+        Map.entry("R_NORM_16", DXGI_FORMAT_R16_SNORM),
+        Map.entry("RGBA_NORM_8", DXGI_FORMAT_R8G8B8A8_SNORM),
+        Map.entry("RG_NORM_8", DXGI_FORMAT_R8G8_SNORM),
+        Map.entry("R_NORM_8", DXGI_FORMAT_R8_SNORM),
+        Map.entry("RGBA_UINT_32", DXGI_FORMAT_R32G32B32A32_UINT),
+        Map.entry("RG_UINT_32", DXGI_FORMAT_R32G32_UINT),
+        Map.entry("R_UINT_32", DXGI_FORMAT_R32_UINT),
+        Map.entry("RGBA_UINT_16", DXGI_FORMAT_R16G16B16A16_UINT),
+        Map.entry("RG_UINT_16", DXGI_FORMAT_R16G16_UINT),
+        Map.entry("R_UINT_16", DXGI_FORMAT_R16_UINT),
+        Map.entry("RGBA_UINT_8", DXGI_FORMAT_R8G8B8A8_UINT),
+        Map.entry("RG_UINT_8", DXGI_FORMAT_R8G8_UINT),
+        Map.entry("R_UINT_8", DXGI_FORMAT_R8_UINT),
+        Map.entry("RGBA_INT_32", DXGI_FORMAT_R32G32B32A32_SINT),
+        Map.entry("RG_INT_32", DXGI_FORMAT_R32G32_SINT),
+        Map.entry("R_INT_32", DXGI_FORMAT_R32_SINT),
+        Map.entry("RGBA_INT_16", DXGI_FORMAT_R16G16B16A16_SINT),
+        Map.entry("RG_INT_16", DXGI_FORMAT_R16G16_SINT),
+        Map.entry("R_INT_16", DXGI_FORMAT_R16_SINT),
+        Map.entry("RGBA_INT_8", DXGI_FORMAT_R8G8B8A8_SINT),
+        Map.entry("RG_INT_8", DXGI_FORMAT_R8G8_SINT),
+        Map.entry("R_INT_8", DXGI_FORMAT_R8_SINT),
+        Map.entry("RGB_FLOAT_11_11_10", DXGI_FORMAT_R11G11B10_FLOAT),
+        Map.entry("RGBA_UNORM_10_10_10_2", DXGI_FORMAT_R10G10B10A2_UNORM),
+        Map.entry("BC1", DXGI_FORMAT_BC1_UNORM),
+        Map.entry("BC2", DXGI_FORMAT_BC2_UNORM),
+        Map.entry("BC3", DXGI_FORMAT_BC3_UNORM),
+        Map.entry("BC4U", DXGI_FORMAT_BC4_UNORM),
+        Map.entry("BC4S", DXGI_FORMAT_BC4_SNORM),
+        Map.entry("BC5U", DXGI_FORMAT_BC5_UNORM),
+        Map.entry("BC5S", DXGI_FORMAT_BC5_SNORM),
+        Map.entry("BC6U", DXGI_FORMAT_BC6H_UF16),
+        Map.entry("BC6S", DXGI_FORMAT_BC6H_SF16),
+        Map.entry("BC7", DXGI_FORMAT_BC7_UNORM)
     );
 
     @Override
