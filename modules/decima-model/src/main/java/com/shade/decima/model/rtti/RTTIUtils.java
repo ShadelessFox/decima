acf47d34502b5346752f8d27d7d324ce52ba0d7f
ShadelessFox <yanuari.channel@gmail.com>
2023-06-19 22:01:39 +0300

    RTTI: Remove redundant check when converting UUID to string

--- modules/decima-model/src/main/java/com/shade/decima/model/rtti/RTTIUtils.java
+++ modules/decima-model/src/main/java/com/shade/decima/model/rtti/RTTIUtils.java
@@ -10,10 +10,6 @@ public class RTTIUtils {
 
     @NotNull
     public static String uuidToString(@NotNull RTTIObject o) {
-        if (!o.type().isInstanceOf("GGUUID")) {
-            throw new IllegalArgumentException("Object is not an instance of GGUUID");
-        }
-
         return "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x".formatted(
             o.i8("Data3"), o.i8("Data2"), o.i8("Data1"), o.i8("Data0"),
             o.i8("Data5"), o.i8("Data4"),
