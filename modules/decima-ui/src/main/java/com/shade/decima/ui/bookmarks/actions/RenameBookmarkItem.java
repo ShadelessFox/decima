a1cdb1ce29f554570a24736be1d803d71f9c550d
ShadelessFox <yanuari.channel@gmail.com>
2024-01-26 20:06:03 +0100

    Bookmarks: Small UX improvements

--- modules/decima-ui/src/main/java/com/shade/decima/ui/bookmarks/actions/RenameBookmarkItem.java
+++ modules/decima-ui/src/main/java/com/shade/decima/ui/bookmarks/actions/RenameBookmarkItem.java
@@ -10,14 +10,15 @@ import com.shade.util.NotNull;
 
 import javax.swing.*;
 
-import static com.shade.decima.ui.menu.MenuConstants.*;
+import static com.shade.decima.ui.menu.MenuConstants.CTX_MENU_BOOKMARKS_GROUP_GENERAL;
+import static com.shade.decima.ui.menu.MenuConstants.CTX_MENU_BOOKMARKS_ID;
 
 @MenuItemRegistration(parent = CTX_MENU_BOOKMARKS_ID, name = "Re&name\u2026", keystroke = "ctrl ENTER", group = CTX_MENU_BOOKMARKS_GROUP_GENERAL, order = 1000)
 public class RenameBookmarkItem extends MenuItem {
     @Override
     public void perform(@NotNull MenuItemContext ctx) {
         final Bookmark bookmark = ctx.getData(BookmarkView.BOOKMARK_KEY);
-        final String name = JOptionPane.showInputDialog("Rename bookmark '%s' to:".formatted(bookmark.name()));
+        final String name = JOptionPane.showInputDialog("Rename bookmark '%s' to:".formatted(bookmark.name()), bookmark.name());
 
         if (name != null && !name.isBlank()) {
             BookmarkManager.getInstance().removeBookmark(bookmark);
