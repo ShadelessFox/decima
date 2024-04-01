f0b545c3015d31ba434981656414753c9b172afe
ShadelessFox <yanuari.channel@gmail.com>
2024-04-01 04:44:20 +0200

    Implement reading of `Pose`

--- modules/decima-model/src/main/java/com/shade/decima/model/rtti/messages/hfw/HFWDebugMouseCursorPS4Handler.java
+++ modules/decima-model/src/main/java/com/shade/decima/model/rtti/messages/hfw/HFWDebugMouseCursorPS4Handler.java
@@ -1,4 +1,4 @@
-package com.shade.decima.model.rtti.messages.shared;
+package com.shade.decima.model.rtti.messages.hfw;
 
 import com.shade.decima.model.base.GameType;
 import com.shade.decima.model.rtti.Type;
@@ -6,28 +6,20 @@ import com.shade.decima.model.rtti.messages.MessageHandler;
 import com.shade.decima.model.rtti.messages.MessageHandlerRegistration;
 import com.shade.decima.model.rtti.objects.RTTIObject;
 import com.shade.decima.model.rtti.registry.RTTITypeRegistry;
-import com.shade.decima.model.rtti.types.RTTITypeArray;
+import com.shade.platform.model.util.BufferUtils;
 import com.shade.util.NotImplementedException;
 import com.shade.util.NotNull;
 
 import java.nio.ByteBuffer;
 
 @MessageHandlerRegistration(message = "MsgReadBinary", types = {
-    @Type(name = "Pose", game = GameType.DS),
-    @Type(name = "Pose", game = GameType.DSDC),
-    @Type(name = "Pose", game = GameType.HZD)
+    @Type(name = "DebugMouseCursorPS4", game = GameType.HFW),
 })
-public class PoseHandler implements MessageHandler.ReadBinary {
+public class HFWDebugMouseCursorPS4Handler implements MessageHandler.ReadBinary {
     @Override
     public void read(@NotNull RTTITypeRegistry registry, @NotNull ByteBuffer buffer, @NotNull RTTIObject object) {
-        if (buffer.get() != 0) {
-            final int count1 = buffer.getInt();
-            object.set("UnknownData1", RTTITypeArray.read(registry, buffer, registry.find("Mat34"), count1));
-            object.set("UnknownData2", RTTITypeArray.read(registry, buffer, registry.find("Mat44"), count1));
-
-            final int count2 = buffer.getInt();
-            object.set("UnknownData3", RTTITypeArray.read(registry, buffer, registry.find("uint32"), count2));
-        }
+        object.set("Unk1", buffer.getInt());
+        object.set("Unk2", BufferUtils.getBytes(buffer, buffer.getInt()));
     }
 
     @Override
@@ -43,10 +35,9 @@ public class PoseHandler implements MessageHandler.ReadBinary {
     @NotNull
     @Override
     public Component[] components(@NotNull RTTITypeRegistry registry) {
-        return new Component[]{
-            new Component("UnknownData1", registry.find("Array<Mat34>")),
-            new Component("UnknownData2", registry.find("Array<Mat44>")),
-            new Component("UnknownData3", registry.find("Array<uint32>")),
+        return new Component[] {
+            new Component("Unk1", registry.find("uint32")),
+            new Component("Unk2", registry.find("Array<uint8>")),
         };
     }
 }
--- modules/decima-model/src/main/java/com/shade/decima/model/rtti/messages/hfw/HFWPoseHandler.java
+++ modules/decima-model/src/main/java/com/shade/decima/model/rtti/messages/hfw/HFWPoseHandler.java
@@ -6,20 +6,26 @@ import com.shade.decima.model.rtti.messages.MessageHandler;
 import com.shade.decima.model.rtti.messages.MessageHandlerRegistration;
 import com.shade.decima.model.rtti.objects.RTTIObject;
 import com.shade.decima.model.rtti.registry.RTTITypeRegistry;
-import com.shade.platform.model.util.BufferUtils;
+import com.shade.decima.model.rtti.types.RTTITypeArray;
 import com.shade.util.NotImplementedException;
 import com.shade.util.NotNull;
 
 import java.nio.ByteBuffer;
 
 @MessageHandlerRegistration(message = "MsgReadBinary", types = {
-    @Type(name = "DebugMouseCursorPS4", game = GameType.HFW),
+    @Type(name = "Pose", game = GameType.HFW)
 })
-public class HFWDebugMouseCursorPS4 implements MessageHandler.ReadBinary {
+public class HFWPoseHandler implements MessageHandler.ReadBinary {
     @Override
     public void read(@NotNull RTTITypeRegistry registry, @NotNull ByteBuffer buffer, @NotNull RTTIObject object) {
-        object.set("Unk1", buffer.getInt());
-        object.set("Unk2", BufferUtils.getBytes(buffer, buffer.getInt()));
+        if (buffer.get() != 0) {
+            final int count1 = buffer.getInt();
+            object.set("UnknownData1", RTTITypeArray.read(registry, buffer, registry.find("Mat34"), count1));
+            object.set("UnknownData2", RTTITypeArray.read(registry, buffer, registry.find("Mat34"), count1));
+
+            final int count2 = buffer.getInt();
+            object.set("UnknownData3", RTTITypeArray.read(registry, buffer, registry.find("uint32"), count2));
+        }
     }
 
     @Override
@@ -35,9 +41,10 @@ public class HFWDebugMouseCursorPS4 implements MessageHandler.ReadBinary {
     @NotNull
     @Override
     public Component[] components(@NotNull RTTITypeRegistry registry) {
-        return new Component[] {
-            new Component("Unk1", registry.find("uint32")),
-            new Component("Unk2", registry.find("Array<uint8>")),
+        return new Component[]{
+            new Component("UnknownData1", registry.find("Array<Mat34>")),
+            new Component("UnknownData2", registry.find("Array<Mat44>")),
+            new Component("UnknownData3", registry.find("Array<uint32>")),
         };
     }
 }
