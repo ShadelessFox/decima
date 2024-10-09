e7dae67b85822a41fc93a6db1b6225285c5ded86
ShadelessFox <yanuari.channel@gmail.com>
2024-10-09 23:32:50 +0200

    Add a test for ensuring correctness of attribute sorting

--- modules/decima-game-until-dawn/src/test/java/com/shade/decima/rtti/RTTITest.java
+++ modules/decima-game-until-dawn/src/test/java/com/shade/decima/rtti/RTTITest.java
@@ -7,14 +7,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
-import static com.shade.decima.rtti.UntilDawn.Vec3;
+import static com.shade.decima.rtti.UntilDawn.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RTTITest {
-    @ParameterizedTest()
+    @ParameterizedTest
     @MethodSource("getTypes")
     void canConstructAndPassEqualityCheck(@NotNull Class<?> iface) {
         var instance = RTTI.newInstance(iface);
@@ -27,6 +28,35 @@ class RTTITest {
             .suppress(Warning.NONFINAL_FIELDS)
             .withIgnoredFields(categories)
             .verify();
+    }
+
+    @Test
+    void canProperlySortAttributes() {
+        List<RTTI.AttributeInfo> attrs;
+
+        // This type has a particularly weird sorting due to the fact that
+        // all its attributes are properties and don't have an offset, so
+        // the test ensures that they get sorted correctly using a dedicated
+        // algorithm used internally by the engine.
+        attrs = RTTI.getAttrsSorted(RenderPass.class);
+        assertEquals(10, attrs.size());
+        assertEquals("HasAlphaTest", attrs.get(0).name());
+        assertEquals("Wireframe", attrs.get(1).name());
+        assertEquals("EnableDepthClamp", attrs.get(2).name());
+        assertEquals("ColorMask", attrs.get(3).name());
+        assertEquals("DepthBias", attrs.get(4).name());
+        assertEquals("UseStencil", attrs.get(5).name());
+        assertEquals("BlendMode", attrs.get(6).name());
+        assertEquals("WriteDepth", attrs.get(7).name());
+        assertEquals("CullMode", attrs.get(8).name());
+        assertEquals("DepthTestFunc", attrs.get(9).name());
+
+        // And for other types, the order should be by offset of their attributes
+        attrs = RTTI.getAttrsSorted(Vec3.class);
+        assertEquals(3, attrs.size());
+        assertEquals("X", attrs.get(0).name());
+        assertEquals("Y", attrs.get(1).name());
+        assertEquals("Z", attrs.get(2).name());
     }
 
     @Test
