d20e65f16b213f4e44318e88619acdace41ae7f3
ShadelessFox <yanuari.channel@gmail.com>
2024-11-13 22:13:55 +0100

    Remove unused code

--- modules/decima-game-until-dawn/src/main/java/com/shade/decima/rtti/test/TypeFactoryTest.java
+++
@@ -1,9 +0,0 @@
-package com.shade.decima.rtti.test;
-
-public class TypeFactoryTest {
-    public static void main(String[] args) {
-        //var factory = new UntilDawnTypeFactory();
-        //var info = factory.get(UntilDawn.Vec3.class);
-        //var object = factory.newInstance(info);
-    }
-}
