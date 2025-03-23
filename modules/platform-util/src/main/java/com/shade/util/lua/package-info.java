1e3f628d3e36567cf4a305c3077546df4f0ff778
ShadelessFox <yanuari.channel@gmail.com>
2025-03-23 19:54:39 +0100

    Fix reference documentation link

--- modules/platform-util/src/main/java/com/shade/util/lua/package-info.java
+++ modules/platform-util/src/main/java/com/shade/util/lua/package-info.java
@@ -1,6 +1,6 @@
 /**
  * This package contains a minimal implementation of Lua's {@code lua_undump}.
  *
- * @see <a href="https://github.com/jrouwe/JoltPhysics">Jolt Physics</a>
+ * @see <a href="https://www.lua.org/source/5.1/lundump.c.html">lundump.c</a>
  */
 package com.shade.util.lua;
